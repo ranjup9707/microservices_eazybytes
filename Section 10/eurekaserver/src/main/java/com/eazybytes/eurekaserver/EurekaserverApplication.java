2df0fe22d02bb55c3c0d4a7babb57cf63048187b
Ranju <ranjupillai2016@gmail.com>
2025-04-12 19:38:42 +0530

    10042025 - Making Microservices Resilient

---
+++ Section 10/eurekaserver/src/main/java/com/eazybytes/eurekaserver/EurekaserverApplication.java
@@ -0,0 +1,17 @@
+package com.eazybytes.eurekaserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+// Converting normal Spring Boot application into Service Discovery Agent with help of Eureka library
+// present in the Spring Cloud Netflix Project
+@EnableEurekaServer
+public class EurekaserverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EurekaserverApplication.class, args);
+	}
+
+}
